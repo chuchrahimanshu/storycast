764eef4a325aff8978801b0c488ecdb42125fb8a
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-07-08 07:46:37 +0530

    Update App Component and Add Color Utility

---
+++ frontend/src/utils/colors.util.ts
@@ -0,0 +1,11 @@
+const colors = {
+  PRIMARY: '#443333',
+  SECONDARY: '#EEA849',
+  CONTRAST: '#FFFFFF',
+  INACTIVE_CONTRAST: 'rgba(255, 255, 255, 0.5)',
+  ERROR: '#FF5B14',
+  SUCCESS: '#3BEF1E',
+  OVERLAY: 'rgba(45, 35, 35, 0.5)',
+};
+
+export {colors};
